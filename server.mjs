ca7fa1ce6a5ef624e93e0160b3d2585023ddc434
ameenstudio <200854608+ameenstudio@users.noreply.github.com>
2025-04-17 13:12:22 -0600

    first commit

---
+++ server.mjs
@@ -0,0 +1,17 @@
+//import
+import express from 'express';
+
+//Setup
+const app = express ();
+const PORT = 3000 || 3001
+
+// Middelware
+
+//Routes
+
+//Listener 
+app.listen(PORT, ()=>{
+
+    console.log(`server runing op Port: ${PORT}`)
+    
+    })
