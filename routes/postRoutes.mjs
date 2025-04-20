5882c5216657df829490a340e83027a1a1720f7d
ameenstudio <200854608+ameenstudio@users.noreply.github.com>
2025-04-19 19:56:46 -0600

    adedd err handlingg

--- routes/postRoutes.mjs
+++ routes/postRoutes.mjs
@@ -5,8 +5,23 @@ import posts from '../data/posts.mjs';
 import comments from '../data/comments.mjs';
 
 const router = express.Router();
+
+//description: this route gtes all posts
+//path : /api/post
+
 router.get('/', (req, res )=>{
-    res.send('Post Routes')
+    res.json(posts)
 })
+
+//description: this route will get one post
+//path : /api/post/
+router.get('/:id',(req,res)=>{
+    const post = posts.find(p=> p.id == req.params.id);
+    if(post)res.json(post)
+        else res.json({error:'page not found'})
+})
+
+
+
 
 export default router;
--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -5,8 +5,22 @@ import users from '../data/users.mjs';
 import posts from '../data/posts.mjs';
 import comments from '../data/comments.mjs';
 
+//description: this route gtes all all users
+//path : /api/user
+
 router.get('/', (req, res )=>{
-    res.send('User Routes')
+    res.json(users)
+})
+
+//description: this route gtes all all users
+//path : /api/user
+router.get('/:id', (req,res)=>{
+    const user = users.find((u)=>u.id ==req.params.id);
+    if (user)res.json(user);
+    else res.json({error:'user not found'});
+
+    //console.log(req.params.id) testing 
+    res.send('test')
 })
 
 export default router;
